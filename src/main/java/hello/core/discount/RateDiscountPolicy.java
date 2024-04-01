2baeb4575bfa14cace3253532a47cfc0d64ff774
cms0403 <cmsjy040317@naver.com>
2024-04-01 22:57:21 +0900

    의존성 주입

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    //ctrl + shift + t 테스트 클래스 만들기
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
